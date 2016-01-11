69c8d0d78ffe22ecb19dbccabd687d5d2e04f116
Stephen Buckley <stephen.alan.buckley@gmail.com>
2016-01-10 22:02:23 -0500

    adding example secrets file with instructions on where to get app permissions

---
+++ secrets.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+    Fill in your secret keys from Twitter here.
+
+    If you don't know your secrets off the top of your head  and need to look them up/get them, here's how. (If you have your oauth token memorized, I'm sorry, but you are a robot for sure.)
+
+    We're going to Twitter's App center, specifically your account page. If you're not already logged in, log in at https://apps.twitter.com/
+
+    Once you're logged in, either click on the app you want to use Twitter Combiner in, or create it. Afterwards you should be on https://apps.twitter.com/app/<your_app_id>
+
+    Click on Keys and Access Tokens in the menu of the page.
+
+    Take your consumer_key and consumer_secret from this page.
+
+    Below, there will be your access tokens. If you don't already have these, create them now.
+
+    Plug those in before their corresponding commas below and you're ready to go!
+
+*/
+
+
+$settings = array(
+    'oauth_access_token' => ,
+    'oauth_access_token_secret' => ,
+    'consumer_key' => ,
+    'consumer_secret' => ,
+);
